2fe5d8f7bf358f4a46ba4edb1442556b3d7180d6
Merel Kuijs <merelke@xs4all.nl>
2024-08-09 02:17:47 +0200

    Fix formatting

--- src/train.py
+++ src/train.py
@@ -4,14 +4,13 @@ import os
 import time
 import argparse
 
-import pandas as pd
-import scipy.io as sio
 import torch
 from sklearn.cluster import KMeans
 
 from model import MultimodalGAN
 from utils import calculate_metrics, check_dir_exist
 METRIC_PRINT = 'metrics: ' + ', '.join(['{:.4f}'] * 7)
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_epochs", type=int, default=6)
 parser.add_argument("--batch_size", type=int, default=4)  # 128
@@ -19,8 +18,6 @@ parser.add_argument("--lr_g", type=float, default=1e-4,  # 1e-4
                     help="adam: learning rate for G")
 parser.add_argument("--lr_d", type=float, default=1e-4,  # 1e-4
                     help="adam: learning rate for D")
-#parser.add_argument("--lr_ae", type=float, default=1e-4,  # 1e-4
-#                    help="adam: learning rate for AE") no need for AE
 parser.add_argument("--b1", type=float, default=0.5,
                     help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999,
@@ -37,6 +34,7 @@ parser.add_argument("--clip_value", type=float, default=0.05,
 
 parser.add_argument("--n_cpu", type=int, default=8,
                     help="# of cpu threads during batch generation")
+parser.add_argument("--shuffle", type=int, default=1)
 parser.add_argument("--seed", type=int, default=2018)
 
 parser.add_argument('--update_p_freq', type=int, default=10)
@@ -47,26 +45,21 @@ parser.add_argument('--log_freq', type=int, default=5)
 parser.add_argument('--test_freq', type=int, default=1)
 parser.add_argument("--pretrain", type=str, default='None',
                     choices=['img', 'txt', 'load_ae', 'load_all', 'None'])
-parser.add_argument("--dataset", type=str, default='masterpraktikum',
-                    choices=['wikipedia', 'nuswide', 'masterpraktikum'])
-parser.add_argument("--data_dir", type=str, default='data/wikipedia/')
-parser.add_argument('--log_dir', type=str, default='log/')
-parser.add_argument('--cpt_dir', type=str, default='cpt/',
+parser.add_argument("--dataset", type=str, default='masterpraktikum')
+parser.add_argument('--log_dir', type=str, default='../log/')
+parser.add_argument('--cpt_dir', type=str, default='../ckpt/',
                     help="dir for saved checkpoint")
 parser.add_argument('--cellplm_model', type=str, default='20230926_85M',
-                    help="path to cell plm model ") # "path to load txt AE checkpoint" changed to txt embeddings instead of AE checkpoint
-parser.add_argument('--h5ad_data', type=str, default='data/Toy/concatenated.h5ad',
-                    help="path to trascriptional data")
+                    help="CellPLM ckpt")
 parser.add_argument('--hugging_face', type=str, default="google/vit-base-patch16-224",
-                    help="path to visual transformer") # "path to load img AE checkpoint" changed to txt embeddings instead of AE checkpoint
-parser.add_argument('--img_path', type=str, default="data/Toy/img/",
-                    help="path to image folder")
-parser.add_argument('--dm2c_cptpath', type=str, default='cpt/',
-                    help="path to load dm2c checkpoint")
+                    help="Hugging Face ViT identifier")
+parser.add_argument('--h5ad_data', type=str, default='data/Toy/concatenated.h5ad',  # TODO
+                    help="path to GEX data")
+parser.add_argument('--img_data', type=str, default="data/Toy/img/",  # TODO
+                    help="path to image data")
+parser.add_argument('--dm2c_cptpath', type=str, default='../ckpt/',
+                    help="path to load DM2C checkpoint")
 args = parser.parse_args()
-
-use_cuda = torch.cuda.is_available()
-device = torch.device('cuda:0' if use_cuda else 'cpu')
 
 # reproducible
 torch.manual_seed(args.seed)
@@ -78,38 +71,17 @@ METRIC_PRINT = 'metrics: ' + ', '.join(['{:.4f}'] * 7)
 
 if __name__ == '__main__':
     config = dict()
-    if args.dataset == 'wikipedia':
-        config['img_input_dim'] = 2048
-        config['txt_input_dim'] = 2048
-        config['n_clusters'] = 10
-        config['img_hiddens'] = [1024, 256, 128]
-        config['txt_hiddens'] = [1024, 256, 128]
-        config['img2txt_hiddens'] = [128, 256, 128]
-        config['txt2img_hiddens'] = [128, 256, 128]
-        # if the data include corresponding filename for each sample feature
-        config['has_filename'] = True
-    elif args.dataset == 'nuswide':
-        config['img_input_dim'] = 1000
-        config['txt_input_dim'] = 1000
-        config['img_hiddens'] = [512, 256, 128]
-        config['txt_hiddens'] = [512, 128]
-        config['img2txt_hiddens'] = [128, 128]
-        config['txt2img_hiddens'] = [128, 128]
-        config['has_filename'] = False
-    # addind a dataset for masterpraktikum
-    elif args.dataset == 'masterpraktikum':
-        # no need for AE
+    if args.dataset == 'masterpraktikum':
         config['img_latent_dim'] = 768
         config['txt_latent_dim'] = 512
-        # config['img_hiddens'] = [512, 256, 128]
-        # config['txt_hiddens'] = [512, 128]
-        # should be between the real dimenstions
+        # hidden dims <= data dims
         config['img2txt_hiddens'] = [512, 512]
         config['txt2img_hiddens'] = [512, 512]
         # config['has_filename'] = False
+    else:
+        raise ValueError()
+    
     config['batchnorm'] = True
-    config['cuda'] = use_cuda
-    config['device'] = device
     current_time = time.strftime(
         "%Y-%m-%d-%H-%M-%S", time.localtime(time.time()))
     config['log_file'] = current_time + '.txt'
@@ -120,34 +92,24 @@ if __name__ == '__main__':
     os.mkdir(args.cpt_dir)
 
     model = MultimodalGAN(args, config)
-    if use_cuda:
+    if torch.cuda.is_available():
         model.to_cuda()
-
-    # pretrain the autoencoders
-    # no need in our case
 
     for epoch in range(args.n_epochs):
         print(epoch)
         model.train(epoch)
+    
     train_embedding = model.embedding(
-        model.train_loader_ordered, unify_modal='txt')
-    print(train_embedding)
-        # test_embedding, test_target, test_modality = model.embedding( no test set as of now
-        #    model.test_loader, unify_modal='img')
-        # no need for kmeans
-        # kmeans = KMeans(config['n_clusters'], max_iter=1000,
-        #                tol=5e-5, n_init=20).fit(train_embedding)
-        # train_metrics = calculate_metrics(train_target, kmeans.labels_)
-        # y_pred = kmeans.predict(test_embedding)
-        # test_metrics = calculate_metrics(test_target, y_pred)
-        # print('>Train', METRIC_PRINT.format(*train_metrics))
-        # print('>Test ', METRIC_PRINT.format(*test_metrics))
-        # sio.savemat('result/result_{}.mat'.format(epoch),
-        #             {'X_embed_train': train_embedding,
-        #              'y_pred_train': kmeans.predict(train_embedding),
-        #              'y_true_train': train_target,
-        #              'modal_train': train_modality,
-        #              'X_embed_test': test_embedding,
-        #              'y_pred_test': y_pred,
-        #              'y_true_test': test_target,
-        #              'modal_test': test_modality})
+        model.train_loader, unify_modal='txt')
+    # TODO: save train_embedding
+    
+    # test_embedding, test_target, test_modality = model.embedding( no test set as of now
+    #    model.test_loader, unify_modal='img')
+    # no need for kmeans
+    # kmeans = KMeans(config['n_clusters'], max_iter=1000,
+    #                tol=5e-5, n_init=20).fit(train_embedding)
+    # train_metrics = calculate_metrics(train_target, kmeans.labels_)
+    # y_pred = kmeans.predict(test_embedding)
+    # test_metrics = calculate_metrics(test_target, y_pred)
+    # print('>Train', METRIC_PRINT.format(*train_metrics))
+    # print('>Test ', METRIC_PRINT.format(*test_metrics))
--- src/utils.py
+++ src/utils.py
@@ -1,76 +1,46 @@
 from __future__ import print_function, absolute_import, division
 
 import os
+import math
 import anndata as ad
 import torch
 import numpy as np
-import scipy.io as sio
 from torch.utils.data import Dataset
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-import math
-#from sklearn.metrics.cluster.supervised import contingency_matrix
-#from munkres import Munkres
-class MFeatDataSet(Dataset):
-    '''Mixed-modal feature'''
+from sklearn.metrics.cluster.supervised import contingency_matrix
+from munkres import Munkres
 
-    def __init__(self, file_mat, has_filename=False):
-        self.file_mat = sio.loadmat(file_mat)
-        self.lens = len(self.file_mat['X'])
-        self.has_filename = has_filename
-
-    def __getitem__(self, index):
-        if self.has_filename:
-            feat, file, modality = self.file_mat['X'][index]
-        else:
-            feat, modality = self.file_mat['X'][index]
-        feat = feat.squeeze().astype(np.float32)
-        cluster_label = self.file_mat['y'][0][index]
-        cluster_label = np.float32(cluster_label) - 1
-        modality_label = np.float32(modality[0])
-
-        return np.float32(index), feat, modality_label, cluster_label
-
-    def __len__(self):
-        return self.lens
-
-
-class SFeatDataSet(Dataset):
-    '''Single modal feature'''
-
-    def __init__(self, file_mat):
-        self.file_mat = sio.loadmat(file_mat)
-        self.lens = len(self.file_mat['X'])
-
-    def __getitem__(self, index):
-        feat = self.file_mat['X'][index][0].squeeze().astype(np.float32)
-        return feat
-
-    def __len__(self):
-        return self.lens
 
 class h5ad_Dataset(Dataset):
     def __init__(self, h5ad_path):
         self.data = self.prepare_h5ad(h5ad_path)
+
     def prepare_h5ad(self, h5ad_path):
         data = ad.read_h5ad(h5ad_path)
         data.obs_names_make_unique()
         return data
 
+
 class img_Dataset(Dataset):
     def __init__(self, img_path):
         names = os.listdir(img_path)
-        self.imgs = [os.path.join(img_path,name) for name in names]
+        self.imgs = [os.path.join(img_path, name) for name in names]
+
     def __getitem__(self, index):
         return self.imgs[index]
+
     def __len__(self):
         return len(self.imgs)
 
-class Custom_Dataloader():
+
+class Custom_Dataloader:
     def __init__(self, dataset, modal, batch_size=32, shuffle=True):
         self.dataset, self.modal, self.shuffle = dataset, modal, shuffle
-        self.batch_size, self.txt_idx, self.img_idx = self._get_indices(batch_size) # update batch_size and get indices
+        self.batch_size, self.txt_idx, self.img_idx = self._get_indices(
+            batch_size
+        )  # update batch_size and get indices
 
     def _get_indices(self, batch_size):
         # get indices for both modalities
@@ -79,12 +49,12 @@ class Custom_Dataloader():
         if self.shuffle:
             txt_idx = np.random.permutation(txt_idx)
             img_idx = np.random.permutation(img_idx)
-        # get the batch size depending on embeddings
-        updated_batch_size = min(int(batch_size/2), len(txt_idx), len(img_idx))
-        return updated_batch_size*2, txt_idx, img_idx
+        # get batch size depending on embedding availability
+        updated_batch_size = min(int(batch_size / 2), len(txt_idx), len(img_idx))
+        return updated_batch_size * 2, txt_idx, img_idx
 
     def __iter__(self):
-        indices = min(len(self.img_idx), len(self.txt_idx))
+        indices = min(len(self.img_idx), len(self.txt_idx))  # TODO
         txt_batch, img_batch = [], []
         for index in range(indices):  # iterate over indices using the iterator
             txt_batch.append(self.dataset[self.txt_idx[index]])
@@ -92,7 +62,11 @@ class Custom_Dataloader():
             if len(txt_batch) + len(img_batch) == self.batch_size:
                 yield torch.stack(txt_batch), torch.stack(img_batch)
                 txt_batch, img_batch = [], []
-        if len(txt_batch) > 0 and len(img_batch) > 0 and len(txt_batch) == len(img_batch): # returning last not full batch
+        if (
+            len(txt_batch) > 0
+            and len(img_batch) > 0
+            and len(txt_batch) == len(img_batch)
+        ):  # returning last batch with size < batch_size
             yield torch.stack(txt_batch), torch.stack(img_batch)
 
     def __len__(self):
@@ -101,15 +75,13 @@ class Custom_Dataloader():
         length = math.ceil(batch_number)
         return length
 
-def run_PCA_on_modal(x, n_feat):
-    ''' have to wait for bigger dataset with this
-        x = x.detach().numpy()
-        x = StandardScaler().fit_transform(x)
-        pca = PCA(n_components=n_feat)
-        x = pca.fit_transform(x)
-        return torch.tensor(x)
-        '''
-    return x[:, :n_feat]  # for no only take the first 512 to match the dimensions
+
+def run_PCA(x, n_feat):
+    x = x.detach().numpy()
+    x = StandardScaler().fit_transform(x)
+    pca = PCA(n_components=n_feat)
+    x = pca.fit_transform(x)
+    return torch.tensor(x)
 
 
 def best_map(L1, L2):
@@ -140,7 +112,9 @@ def get_ar(y_true, y_pred):
 
 
 def get_nmi(y_true, y_pred):
-    return metrics.normalized_mutual_info_score(y_true, y_pred, average_method='arithmetic')
+    return metrics.normalized_mutual_info_score(
+        y_true, y_pred, average_method="arithmetic"
+    )
 
 
 def get_fpr(y_true, y_pred):
@@ -149,16 +123,19 @@ def get_fpr(y_true, y_pred):
     tk = np.dot(c.data, np.transpose(c.data)) - n_samples  # TP
     pk = np.sum(np.asarray(c.sum(axis=0)).ravel() ** 2) - n_samples  # TP+FP
     qk = np.sum(np.asarray(c.sum(axis=1)).ravel() ** 2) - n_samples  # TP+FN
-    precision = 1. * tk / pk if tk != 0. else 0.
-    recall = 1. * tk / qk if tk != 0. else 0.
-    f = 2 * precision * recall / (precision + recall) if (precision +
-                                                          recall) != 0. else 0.
+    precision = 1.0 * tk / pk if tk != 0.0 else 0.0
+    recall = 1.0 * tk / qk if tk != 0.0 else 0.0
+    f = (
+        2 * precision * recall / (precision + recall)
+        if (precision + recall) != 0.0
+        else 0.0
+    )
     return f, precision, recall
 
 
 def get_purity(y_true, y_pred):
     c = metrics.confusion_matrix(y_true, y_pred)
-    return 1. * c.max(axis=0).sum() / np.shape(y_true)[0]
+    return 1.0 * c.max(axis=0).sum() / np.shape(y_true)[0]
 
 
 def calculate_metrics(y, y_pred):
